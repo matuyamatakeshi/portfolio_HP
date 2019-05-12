dae1c3fd6d45d0bb4e7fca717e3eae0dcd2f033a
takeshi <takeshimail>
2019-05-12 16:57:56 +0900

    tesuto

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(".slider").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3400
+});
+
+$('.mitem').mousemove(function () {
+    $(this).css("color", "#0ff")
+});
+
+$(".mitem").mouseout(function () {
+    $(this).css("color", "white")
+});
+
+$(".hitem1").mousemove(function () {
+    $(this).text("~WELCOME!!~")
+});
+
+$(".hitem1").mouseout(function () {
+    $(this).text("~World Heritage~")
+});
