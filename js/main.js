f18e796516d04e960d7eeee9f43225cb847a05d3
takeshi <takeshimail>
2019-05-20 19:43:44 +0900

    pegetopボタンの追加

--- js/main.js
+++ js/main.js
@@ -25,4 +25,22 @@ $(".hitem1").mouseout(function () {
     $(this).text("~World Heritage~")
 });
 
+$(function () {
+    var topBtn = $('.page-top');
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
 
